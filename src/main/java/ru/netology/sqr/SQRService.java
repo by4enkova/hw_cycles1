07d8e30d9a013aa98b50d79ccb77b3856e9ccf10
Darya Bychenkova <by4enkova1@gmail.com>
2022-02-13 20:06:02 +0300

    Tests are added

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -4,13 +4,11 @@ public class SQRService {
 
     public int sqrtCeil(int numMin, int numMax) {
         int counter = 0;
-        for (int i = 10; i < 99; i++) {
-            if (i * i >= numMin) {
-                if (i * i <= numMax) {
+        for (int i = 10; i <= 99; i++) {
+            if (i * i >= numMin && i * i <= numMax) {
                     counter++;
                 }
             }
-        }
         return counter;
+        }
     }
-}
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -13,4 +13,44 @@ class SQRServiceTest {
         int actual = service.sqrtCeil(200, 300);
         assertEquals(expected, actual);
     }
+
+    @Test
+    void sqrtCeilAnotherRange() {
+        SQRService service = new SQRService();
+        int expected = 11;
+        int actual = service.sqrtCeil(100, 400);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void sqrtCeilMaxNumber() {
+        SQRService service = new SQRService();
+        int expected = 1;
+        int actual = service.sqrtCeil(9700, 9900);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void sqrtCeilUnderMaxNumber() {
+        SQRService service = new SQRService();
+        int expected = 0;
+        int actual = service.sqrtCeil(9900, 10000);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void sqrtCeilMinNumber() {
+        SQRService service = new SQRService();
+        int expected = 1;
+        int actual = service.sqrtCeil(80, 110);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void sqrtCeilUnderMinNumber() {
+        SQRService service = new SQRService();
+        int expected = 0;
+        int actual = service.sqrtCeil(40, 99);
+        assertEquals(expected, actual);
+    }
 }
